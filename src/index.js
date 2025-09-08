507dd6a6a1f55caeb833ee48bee2ab4cf9902956
cuiminghao <1186397167@qq.com>
2025-09-08 14:55:53 +0800

    添加简写路径

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import sum from "@/sum";
+
+console.log(sum(5, 6));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
