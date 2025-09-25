41eeaa89211b55a6447bb259354c11ec8866b841
cuiminghao <1186397167@qq.com>
2025-09-25 18:41:16 +0800

    25号的所有，新增一笔页面，日记账

--- src/pages/Layout/Month/components/daybill/index.js
+++ src/pages/Layout/Month/components/daybill/index.js
@@ -1,8 +1,11 @@
 import classNames from "classnames";
 import "./index.scss";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { billTypeToName } from "@/constants";
+import Icon from "../icon";
 
 const DailyBill = ({ data, date }) => {
+  const [visible, setvisible] = useState(false);
   console.log(data);
   console.log(date);
   const resultBill = useMemo(() => {
@@ -27,7 +30,10 @@ const DailyBill = ({ data, date }) => {
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames("arrow")}></span>
+          <span
+            className={classNames("arrow", visible && "expand")}
+            onClick={() => setvisible(!visible)}
+          ></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -43,6 +49,21 @@ const DailyBill = ({ data, date }) => {
             <span className="type">结余</span>
           </div>
         </div>
+      </div>
+      <div className="billList" style={{ display: visible ? "block" : "none" }}>
+        {data.map((item) => {
+          return (
+            <div className="bill" key={item.id}>
+              <Icon type={item.useFor}></Icon>
+              <div className="detail">
+                <div className="billType">{billTypeToName[item.useFor]}</div>
+              </div>
+              <div className={classNames("money", item.type)}>
+                {item.money.toFixed(2)}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,112 @@
+import { Button, DatePicker, Input, NavBar } from "antd-mobile";
+import Icon from "@/pages/Layout/Month/components/icon";
+import "./index.scss";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
+import { billListData } from "@/constants";
+import { useState } from "react";
+
 const New = () => {
-  return <div>我是New</div>;
+  const navigate = useNavigate();
+  const [billtype, setBillTyle] = useState("pay");
+  const [money, setMoney] = useState(0);
+  const moneyChange = (e) => {
+    setMoney(e.target.value);
+  };
+  const saveData = () => {
+    console.log({
+      type: billtype,
+      money: money,
+    });
+    return {
+      type: billtype,
+      money: money,
+    };
+  };
+  return (
+    <div className="keepAccounts">
+      <NavBar className="nav">记一笔</NavBar>
+
+      <div className="header">
+        <div className="kaType">
+          <Button
+            shape="rounded"
+            className={classNames(billtype === "pay" ? "selected" : "")}
+            onClick={() => {
+              setBillTyle("pay");
+            }}
+          >
+            支出
+          </Button>
+          <Button
+            className={classNames(billtype === "income" ? "selected" : "")}
+            shape="rounded"
+            onClick={() => {
+              setBillTyle("income");
+            }}
+          >
+            收入
+          </Button>
+        </div>
+
+        <div className="kaFormWrapper">
+          <div className="kaForm">
+            <div className="date">
+              <Icon type="calendar" className="icon" />
+              <span className="text"></span>
+              {/* 时间选择器 */}
+              <DatePicker
+                className="kaDate"
+                title="记账日期"
+                max={new Date()}
+              />
+            </div>
+            <div className="kaInput">
+              <Input
+                className="input"
+                placeholder="0.00"
+                type="number"
+                value={money}
+                onChange={(e) => {
+                  console.log(e);
+                  setMoney(e);
+                }}
+              />
+              <span className="iconYuan">¥</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="kaTypeList">
+        {/* 数据区域 */}
+        {billListData[billtype].map((item) => {
+          return (
+            <div className="kaType" key={item.type}>
+              <div className="title">{item.name}</div>
+              <div className="list">
+                {item.list.map((item) => {
+                  return (
+                    // selected
+                    <div className={classNames("item", "")} key={item.type}>
+                      <div className="icon">
+                        <Icon type={item.type} />
+                      </div>
+                      <div className="text">{item.name}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="btns">
+        <Button className="btn save" onClick={saveData}>
+          保 存
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default New;
